f51f354b43803bf255d5fa07c877f7d3520d18c8
Bektas Keldibayev <bektas.keldibayev@nu.edu.kz>
2024-11-25 21:53:01 +0500

    add azure database connection

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,13 +1,30 @@
+import fs from "fs";
+import path from "path";
+import "dotenv/config";
 import pkg from "pg";
 const { Pool } = pkg;
-import "dotenv/config";
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  user: process.env.DATABASE_USER,
+  host: process.env.DATABASE_HOST,
+  database: "healthcare",
+  password: process.env.DATABASE_PASSWORD,
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-pool.on("connect", () => {
-  console.log("Connected to PostgreSQL...");
-});
+(async () => {
+  try {
+    const client = await pool.connect();
+    console.log("Connected to the database successfully!");
+    const res = await client.query("SELECT NOW()");
+    console.log("Current Time:", res.rows[0]);
+    client.release();
+  } catch (err) {
+    console.error("Database connection error:", err.stack);
+  }
+})();
 
 export default pool;
